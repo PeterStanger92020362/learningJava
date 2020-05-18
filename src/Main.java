98dcfebc8cc819f6f23d4eb8d4df7d41d2230109
Peter Stanger <psta191@mywhitecliffe.com>
2020-05-18 20:33:59 +1200

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+
+import java.awt.Color;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Peter
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        
+       Car myCar = new Car(25.5,"1BC32E",Color.BLUE,true);
+
+    }
+}
